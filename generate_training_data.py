fea17c4a382a4ea02b3c605b43404f259dc02107
dOsan3 <>
2023-04-27 22:58:13 -0400

    Saving progress.

--- generate_training_data.py
+++ generate_training_data.py
@@ -37,19 +37,54 @@ def expand_answers(questions_and_answers):
     # How much will Happy Org sponsor for the chatbot development?
     for i, qa_pair in enumerate(questions_and_answers):
         #question = qa_pair["question"]
-        # To be replaced with `generate_answers` output
         question = "What is the purpose of the Osan3 Alpha Program?"
-        # ["a1", "a2", ...]
+        # To be replaced with `generate_more_answers` output
         more_answers = generate_more_answers(question)
+        more_answers = [
+            "The purpose of the Osan3 Alpha Program is to develop a custom chatbot designed to meet the specific needs of Happy Org or its target industry. The program aims to create a tailored solution that effectively integrates with Happy Org's existing systems, streamlines efficiency, and enhances the user experience when navigating Happy Org products. By partnering with Happy Org, Osan3 also seeks to foster innovation and collaboration, ultimately promoting the benefits of the chatbot solution within the target industry.",
+            "The purpose of the Osan3 Alpha Program is to foster the development of custom chatbot solutions tailored to meet the specific needs of organizations, like Happy Org, or their target industries. The program aims to create innovative, effective, and user-friendly chatbot systems that integrate seamlessly with the existing systems of the participating organizations, streamline efficiency, and enhance user experience when navigating their products. Through partnerships, sponsorships, and collaboration, the Osan3 Alpha Program seeks to drive innovation and growth in chatbot technology and applications, while promoting the benefits of these solutions within the target industries.",
+            "The purpose of the Osan3 Alpha Program is to foster the development of innovative chatbot solutions tailored to meet the specific needs of Happy Org or its target industry. By providing financial support and collaborating with Osan3, Happy Org aims to create a custom chatbot that effectively integrates with its existing systems, streamlines efficiency, and enhances the user experience when navigating Happy Org products. Through the program, both parties will work together to promote the benefits of the chatbot solution within the target industry, positioning Happy Org as an industry innovator and encouraging user engagement.",
+            "The purpose of the Osan3 Alpha Program is to foster the development of innovative chatbot solutions tailored to meet the specific needs of Happy Org or its target industry. By providing financial support and collaborating with Osan3, Happy Org aims to create a custom chatbot that effectively integrates with its current systems, streamlines efficiency, and enhances the user experience when navigating Happy Org products. Through the program, both parties will work together to promote the benefits of the chatbot solution within the target industry, positioning Happy Org as an industry innovator and encouraging user engagement."
+         ]
 
         for answer in more_answers:
             training_data.append(create_training_data_entry(i, question, answer))
 
     return training_data
+    # To be replaced with results from for loop above results in training_data
+    return {
+      "id": "osan3-happy-org_1",
+      "conversations": [
+        {
+        "from": "human",
+        "value": "What is the purpose of the Osan3 Alpha Program?"
+        },
+        {
+        "from": "gpt",
+        "value": "The purpose of the Osan3 Alpha Program is to develop a custom chatbot designed to meet the specific needs of Happy Org or its target industry. The program aims to create a tailored solution that effectively integrates with Happy Org's existing systems, streamlines efficiency, and enhances the user experience when navigating Happy Org products. By partnering with Happy Org, Osan3 also seeks to foster innovation and collaboration, ultimately promoting the benefits of the chatbot solution within the target industry.",
+        },
+        {
+        "from": "human",
+        "value": "What is the purpose of the Osan3 Alpha Program?"
+        },
+        {
+        "from": "gpt",
+        "value": "The purpose of the Osan3 Alpha Program is to foster the development of custom chatbot solutions tailored to meet the specific needs of organizations, like Happy Org, or their target industries. The program aims to create innovative, effective, and user-friendly chatbot systems that integrate seamlessly with the existing systems of the participating organizations, streamline efficiency, and enhance user experience when navigating their products. Through partnerships, sponsorships, and collaboration, the Osan3 Alpha Program seeks to drive innovation and growth in chatbot technology and applications, while promoting the benefits of these solutions within the target industries.",
+        },
+        {
+        "from": "human",
+        "value": "What is the purpose of the Osan3 Alpha Program?"
+        },
+        {
+        "from": "gpt",
+        "value": "The purpose of the Osan3 Alpha Program is to foster the development of innovative chatbot solutions tailored to meet the specific needs of Happy Org or its target industry. By providing financial support and collaborating with Osan3, Happy Org aims to create a custom chatbot that effectively integrates with its current systems, streamlines efficiency, and enhances the user experience when navigating Happy Org products. Through the program, both parties will work together to promote the benefits of the chatbot solution within the target industry, positioning Happy Org as an industry innovator and encouraging user engagement."
+        },
+      ]
+    }
 
 def create_training_data_entry(id, question, answer):
     return {
-        "id": f"identity_{id}",
+        "id": f"osan3-happy-org_{id}",
         "conversations": [
             {
                 "from": "human",
