dc795ffe7a82094b710fec9f01469b8b255a8dc2
dOsan3 <>
2023-04-28 10:43:40 -0400

    Fix so adds answers and questions in the right doc-id key convo.

--- populate-training-data.py
+++ populate-training-data.py
@@ -1,23 +1,31 @@
 import json
 
+# Store all JSON objects in a dictionary with 'doc_name-id' keys
+json_objects = {}
+
 def create_json_object(doc_name, id):
     return {
         "id": f"{doc_name}_{id}",
         "conversations": []
     }
 
-def add_conversation(json_object, question, answer):
-    json_object["conversations"].append(question)
-    json_object["conversations"].append(answer)
+def add_conversation(doc_name, id, question, answer):
+    key = f"{doc_name}_{id}"
+    if key not in json_objects:
+        json_objects[key] = create_json_object(doc_name, id)
+    json_objects[key]["conversations"].append(question)
+    json_objects[key]["conversations"].append(answer)
 
 def main(doc_name, id, question, answer):
-    json_object = create_json_object(doc_name, id)
-    add_conversation(json_object, question, answer)
-    print(json.dumps(json_object, indent=2))
+    add_conversation(doc_name, id, question, answer)
+    print(json.dumps(json_objects, indent=2))
 
 if __name__ == "__main__":
     doc_name = "osan3-happy-org"  # Replace with the desired document name
     id = "your_id_here"          # Replace with the desired ID value
     question = "Your question here"  # Replace with the desired question
     answer = "Your answer here"      # Replace with the desired answer
+    main(doc_name, id, question, answer)
+    question = "Your question here"  # Replace with the desired question
+    answer = "Your 2nd answer here"      # Replace with the desired answer
     main(doc_name, id, question, answer)
